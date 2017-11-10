a02a94f7d477fe682893c38a360206df5ba5d9a7
vothikimchi <vtkchi1297@gmail.com>
2017-11-10 08:58:39 +0700

    HomeController

--- PPcRentalProject/PPcRentalProject/Controllers/HomeController.cs
+++ PPcRentalProject/PPcRentalProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace PPcRentalProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Helllllo";
 
             return View();
         }
